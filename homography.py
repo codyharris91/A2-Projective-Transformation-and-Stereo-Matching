e7011753fc62fdc5f160d229a7c4265568cc6a64
Cody Harris <31891287+codyharris91@users.noreply.github.com>
2021-04-18 20:27:41 -0400

    step 2 solved

--- homography.py
+++ homography.py
@@ -15,6 +15,7 @@
 import sys
 import os
 import cv2
+import numpy as np
 
 def draw_circle(event, x, y, flags, param):
     height = param[0].shape[0]
@@ -27,6 +28,55 @@ def draw_circle(event, x, y, flags, param):
         param[1].append(x)
         param[2].append(y)
 
+def make_xy(x,y):
+    x = np.array(x)
+    y = np.array(y)
+    left_ind = x.argsort()[:2]
+    right_ind = x.argsort()[2:]
+    left_points = [(x[left_ind[0]], y[left_ind[0]]), (x[left_ind[1]], y[left_ind[1]])]
+    right_points = [(x[right_ind[0]], y[right_ind[0]]), (x[right_ind[1]], y[right_ind[1]])]
+    #determine top left and bottom left points
+    if left_points[0][1] > left_points[1][1]:
+        top_left = left_points[1]
+        bottom_left = left_points[0]
+    else:
+        top_left = left_points[0]
+        bottom_left = left_points[1]
+    #determine top right and bottom right points
+    if right_points[0][1] > right_points[1][1]:
+        top_right = right_points[1]
+        bottom_right = right_points[0]
+    else:
+        top_right = right_points[0]
+        bottom_right = right_points[1]
+    return (top_left, top_right, bottom_right, bottom_left)
+
+def get_point_mat(p, p_prime):
+    x = p[0]
+    y = p[1]
+    x_p = p_prime[0]
+    y_p = p_prime[1]
+    p_i = np.array([[-x, -y, -1,  0,  0,  0, x*x_p, y*x_p, x_p],
+                    [ 0,  0,  0, -x, -y, -1, x*y_p, y*y_p, y_p]])
+    return p_i
+
+def solve_h(p, p_prime):
+    A = np.array([])
+    for i in range(len(p)):
+        p_i = get_point_mat(p[i], p_prime[i])
+        if A.size == 0:
+            A = p_i
+        else:
+            A = np.concatenate((A, p_i))
+    last = np.array([0, 0, 0, 0, 0, 0, 0, 0, 1])
+    A = np.append(A, [last], axis = 0)
+    return A.reshape((3,3))
+
+def get_new_xy(x, y, h):
+    pretrans = h.dot([x,y,1])
+    loc = pretrans/pretrans[2]
+    return (loc[0], loc[1])
+
 def main():
     ny_file = sys.argv[1]
     bb_file = sys.argv[2]
@@ -34,6 +84,9 @@ def main():
     #Source: https://www.kite.com/python/examples/4300/os-remove-the-file-extension-from-a-filename
     output = os.path.splitext(ny_file)[0] + '_output.jpg'
     image = cv2.imread(ny_file, -1)
+    bb_img = cv2.imread(bb_file, -1)
+    bb_height = bb_img.shape[0]
+    bb_width = bb_img.shape[1]
 
     x_vals = []
     y_vals = []
@@ -51,9 +104,17 @@ def main():
     if len(x_vals) != 4:
         raise Exception('Please Click Exactly 4 Corners')
 
-    
-    print(x_vals)
-    print(y_vals)
+
+    # Corner order:
+    # 1 > 2
+    # 4 < 3
+    ny_corn = make_xy(x_vals, y_vals)
+    bb_corn = ((0, 0), (bb_width, 0), (bb_width, bb_height), (0, bb_height))
+
+    homog = solve_h(bb_corn, ny_corn)
+
+    print(ny_corn)
+    print(bb_corn)
 
 
 
